e551e7ad6a2a914fe62b08ab74e2951d7d11ba3c
Ryan Everett <ryaneverett33@outlook.com>
2018-09-16 14:59:32 -0400

    Basic Routing Implemented - Added the 3 basic components (Login, Homescreen, and Note) - Will update documentation for each component

---
+++ electron/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { HomescreenComponent } from './homescreen/homescreen.component';
+import { LoginComponent } from './login/login.component';
+import { NoteComponent } from './note/note.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'homescreen', component: HomescreenComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'note', component: NoteComponent },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { 
+  
+}
