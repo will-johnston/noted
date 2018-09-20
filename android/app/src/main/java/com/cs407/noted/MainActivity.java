d1d3baffa8171a2743d99b23212c3455c2276318
Kyle <kyle.n.burke@gmail.com>
2018-09-19 22:42:06 -0400

    picture chooser and image name

--- android/app/src/main/java/com/cs407/noted/MainActivity.java
+++ android/app/src/main/java/com/cs407/noted/MainActivity.java
@@ -3,7 +3,12 @@ package com.cs407.noted;
 import android.content.Intent;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +21,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.getbase.floatingactionbutton.FloatingActionButton;
@@ -36,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter listAdapter;
     private Folder root;
+    private static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         getSupportActionBar().setDisplayShowHomeEnabled(false);
+        firebaseAuth = FirebaseAuth.getInstance();
         recyclerView = findViewById(R.id.recycler_view);
         root = new Folder("noted", R.drawable.folder,null);
         listAdapter = new ListAdapter(null, root);
@@ -79,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
         final FloatingActionsMenu floatingActionsMenu = (FloatingActionsMenu) findViewById(R.id.fam);
         final FloatingActionButton action_folder = (FloatingActionButton) findViewById(R.id.action_folder);
         final FloatingActionButton action_doc = (FloatingActionButton) findViewById(R.id.action_doc);
+        final FloatingActionButton action_select_image = (FloatingActionButton) findViewById(R.id.action_select_image);
         action_folder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -147,8 +156,16 @@ public class MainActivity extends AppCompatActivity {
                 builder.show();
             }
         });
-
-        firebaseAuth = FirebaseAuth.getInstance();
+        action_select_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                floatingActionsMenu.collapse();
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
     }
 
     @Override
@@ -167,7 +184,6 @@ public class MainActivity extends AppCompatActivity {
             //Snackbar.make(findViewById(R.id.main_layout), "Logged in as " + displayName, Snackbar.LENGTH_SHORT).show();
         }
     }
-
 
     public void toggleHomeButton(boolean enable) {
         getSupportActionBar().setDisplayHomeAsUpEnabled(enable);
@@ -189,5 +205,50 @@ public class MainActivity extends AppCompatActivity {
         //show login activity
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PICK_IMAGE) {
+            Uri uri = data.getData();
+
+            /*String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String filePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            Bitmap image = BitmapFactory.decodeFile(filePath);*/
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Enter Image Name");
+
+            // Set up the input
+            final EditText input = new EditText(MainActivity.this);
+            input.setInputType(InputType.TYPE_CLASS_TEXT);
+            builder.setView(input);
+
+            // Set up the buttons
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String input_text = input.getText().toString();
+                    int icon = R.drawable.image;
+                    ListItem item = new ListItem(input_text, icon);
+                    listAdapter.addItemToList(item);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            builder.show();
+        }
     }
 }
