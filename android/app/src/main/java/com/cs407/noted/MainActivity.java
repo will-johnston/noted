f39c5c1bbd839dcdcc5c63874bb2924c90e37868
Kyle <kyle.n.burke@gmail.com>
2018-09-20 16:09:05 -0400

    image preview and permissions

--- android/app/src/main/java/com/cs407/noted/MainActivity.java
+++ android/app/src/main/java/com/cs407/noted/MainActivity.java
@@ -1,8 +1,12 @@
 package com.cs407.noted;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,6 +16,8 @@ import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -24,6 +30,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
@@ -41,6 +48,8 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import static java.security.AccessController.getContext;
+
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
@@ -49,6 +58,12 @@ public class MainActivity extends AppCompatActivity {
     private Folder root;
     private static final int PICK_IMAGE = 1;
     private static final int TAKE_PICTURE = 2;
+    private static final int CAMERA_REQUEST = 3;
+    private static final int WRITE_EXTERNAL_STORAGE_REQUEST = 4;
+    private static final int BOTH_REQUEST = 5;
+    private Uri imageUri;
+    private File output=null;
+    private Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -177,16 +192,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 floatingActionsMenu.collapse();
+
                 StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
                 StrictMode.setVmPolicy(builder.build());
 
                 try {
                     File tempFile = File.createTempFile("my_app", ".jpg");
-                    Uri uri = Uri.fromFile(tempFile);
+                    imageUri = Uri.fromFile(tempFile);
 
-                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-                    startActivityForResult(takePictureIntent, TAKE_PICTURE);
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File dir =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+                    output = new File(dir, "picture.jpeg");
+                    imageUri = Uri.fromFile(output);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, TAKE_PICTURE);
                 }
                 catch(IOException e) {
 
@@ -209,6 +228,19 @@ public class MainActivity extends AppCompatActivity {
         else {
             String displayName = currentUser.getDisplayName();
             //Snackbar.make(findViewById(R.id.main_layout), "Logged in as " + displayName, Snackbar.LENGTH_SHORT).show();
+        }
+
+        int cameraPermission = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA);
+        int storagePermission = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if(cameraPermission == PackageManager.PERMISSION_DENIED && storagePermission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, BOTH_REQUEST);
+        }
+        else if(cameraPermission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST);
+        }
+        else if(storagePermission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_STORAGE_REQUEST);
         }
     }
 
@@ -278,7 +310,14 @@ public class MainActivity extends AppCompatActivity {
             builder.show();
         }
         else if(requestCode == TAKE_PICTURE) {
-            Toast.makeText(getApplicationContext(), "done", Toast.LENGTH_LONG).show();
+            try {
+                imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                //ImageView iv = (ImageView) findViewById(R.id.image);
+                //iv.setImageBitmap(imageBitmap);
+            }
+            catch (IOException e) {
+
+            }
         }
     }
 }
